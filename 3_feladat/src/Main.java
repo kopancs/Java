6bdeaf2b475a900ff1f4267839191bc296dab5fe
Anikó Vári <aniko.kovacs.galamb@gmail.com>
2018-10-06 11:13:02 +0200

    már szép is

--- 3_feladat/src/Main.java
+++ 3_feladat/src/Main.java
@@ -7,12 +7,11 @@ public class Main {
 		int c = Integer.parseInt(args[2]);
 		int d = Integer.parseInt(args[3]);
 		int e = Integer.parseInt(args[4]);
-				
-		int avarage=(a+b+c+d+e)/5;
-		System.out.println("a: "+args[0]+", b: "+args[1]+", c: "+args[2]+", d: "+args[3]+", e: "+args[4]);
-		System.out.println("A számok számtani közepe: "+avarage);
-		
-		
+
+		int avarage = (a + b + c + d + e) / 5;
+		System.out.println(
+				"a: " + args[0] + ", b: " + args[1] + ", c: " + args[2] + ", d: " + args[3] + ", e: " + args[4]);
+		System.out.println("A számok számtani közepe: " + avarage);
 
 	}
 
