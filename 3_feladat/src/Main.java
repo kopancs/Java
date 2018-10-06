dfc33e2a5d5269604be484bd01c0e68ffbf115d1
Anikó Vári <aniko.kovacs.galamb@gmail.com>
2018-10-06 10:58:56 +0200

    init

---
+++ 3_feladat/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		int d = Integer.parseInt(args[3]);
+		int e = Integer.parseInt(args[4]);
+		
+		int avarage=(a+b+b-c+d+e)/5;
+		System.out.println("A számok számtani közepe: "+avarage);
+		
+		
+
+	}
+
+}
